0383942e6cf6bb819814854c7948476bd7625018
luktvpl <luktvpl@wp.pl>
2022-01-16 15:27:25 +0100

    beta 4

--- js/app.js
+++ js/app.js
@@ -29,4 +29,28 @@ const showCoffees = () => {
         .catch(err => console.log("service worker not registered", err))
     })
   }
-
+function test() {
+  alert("hej")
+}
+/*manipulacja plikami cookie*/
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
